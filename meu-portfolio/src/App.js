72e0c1479eae7b3ec75c928936b14f80387bfd18
isisestevan <isisestevan@gmail.com>
2025-02-27 20:40:55 -0300

    ajuste

--- meu-portfolio/src/App.js
+++ meu-portfolio/src/App.js
@@ -1,22 +1,11 @@
-import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Header />
-      <main>
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
