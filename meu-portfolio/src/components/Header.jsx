89e0d1d82e2fb1716e25b4cf4f8127c1b83d45b4
isisestevan <isisestevan@gmail.com>
2025-02-27 21:40:08 -0300

    fix: ajuste estrutura

---
+++ meu-portfolio/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+      <header className='fixed w-full flex jus'>
+        <a href='#'>Isis</a>
+        <nav>
+            <a href='#'>Início</a>
+            <a href='#'>Projetos</a>
+            <a href='#'>Skils</a>
+            <a href='#'>Educação</a>
+            <a href='#'>Contato</a>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
