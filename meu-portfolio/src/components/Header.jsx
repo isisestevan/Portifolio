79840bed8810f28654c3b5adab6e9eb60f6f0320
isisestevan <isisestevan@gmail.com>
2025-03-12 22:35:19 -0300

    feat: adição animação no titulo

--- meu-portfolio/src/components/Header.jsx
+++ meu-portfolio/src/components/Header.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 
+const navList =[
+  {
+    id: 1,
+    data:"Início"
+  },
+  {
+    id: 2,
+    data:"Projetos"
+  },
+  {
+    id: 3,
+    data:"Skills"
+  },
+  {
+    id: 4,
+    data:"Educação"
+  },
+  {
+    id: 5,
+    data:"Contato"
+  }
+]
 
 const Header = () => {
   return (
-    <div>
+   
       <header className='fixed w-full flex justify-between items-center p-4 z-50 bg-transparent'>
-        <a href='#' className='text-3xl font-extrabold text-black-500 hover:scale-110'>Isis Estevan</a>
+        <a href='#' className='text-3xl font-extrabold text-black-500 hover:scale-110 transition duration-300'>Isis Estevan</a>
         <nav>
-            <a href='#'>Início</a>
-            <a href='#'>Projetos</a>
-            <a href='#'>Skils</a>
-            <a href='#'>Educação</a>
-            <a href='#'>Contato</a>
+          {navList.map((item) =>(
+          <a key={item.id} href='#' className='ml-8 text-lg font-medium text-black hover:text-red-500 border-b-2 border-transparent hover:border-red-500'>{item.data}</a>
+          ))}
+          
+            
         </nav>
       </header>
-    </div>
+    
   )
 }
 
--- meu-portfolio/src/components/Home.jsx
+++ meu-portfolio/src/components/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import ProfileImage from "../assets/git-profile.png";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="flex flex-col md:flex-row items-center justify-center min-h-screen px-6">
+      <div className="md:w-1/2 flex justify-center">
+        <img src={ProfileImage} alt="GitProfile" className="rounded-full w-48 sm:w-64 md:w-80 lg:w-[380px] xl:w-[480px] shadow-lg 
+        hover:scale-110 transition duration-300"/>
+      </div>
 
-export default Home
+      <div className="md: w-1/2 text-center md:text-left mt-6 md:mt-0">
+        <h1 className="text-4xl sm:text-6xl font-bold mb-4">Oi, aqui é a <span className="text-green-900">Isis</span></h1>
+        <h3 className="text-2xl sm:text-3xl font-semibold mb-4">Sou <span className="text-green-900">
+            
+            <TypeAnimation
+            sequence={
+              ["Cientista da Computação",1000,
+              "Dev FullStack",1000,
+              "Entusiasta de Machine Learning",1000
+            ]
+            } 
+
+            speed={50}
+            style={{fontSize: "lre"}}
+            repeat={Infinity}
+            
+            />
+            </span></h3>
+        <p className="text-sm sm:text-lg mb-6">Sou dev Fullstack, formada em Ciência da Computação pela Universidade de Itaúna, 
+          e sou fascinada por Machine Learning. Quando não tô codando, tô na cozinha testando receitas, 
+          maratonando filmes (time Marvel) ou curtindo a natureza pra dar aquela equilibrada. 
+          Gosto de resolver problemas com tecnologia e criar soluções que realmente fazem a diferença. 
+          Ah, e se tem IA no meio, já ganhou minha atenção!  </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
