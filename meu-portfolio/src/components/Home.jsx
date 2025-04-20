2feda260d33001210c358442c693282464c6843b
isisestevan <isisestevan@gmail.com>
2025-04-20 16:29:04 -0300

    feat: adição melhorias e layout em home

--- meu-portfolio/src/components/Home.jsx
+++ meu-portfolio/src/components/Home.jsx
@@ -1,65 +1,64 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import "../styles/home.css"; // Importa o CSS com os ícones
+import "../styles/home.css";
 
 const Home = () => {
   return (
     <section className="home-section">
-    <div className="home-content">
-      <div className="home-text">
-        <h1 className="home-title">
-          Oi, aqui é a <span className="highlight-name">Isis</span>
-        </h1>
-        <h3 className="home-subtitle">
-          Sou{" "}
-          <span className="highlight">
-            <TypeAnimation
-              sequence={[
-                "Cientista da Computação.",
-                1000,
-                "Dev FullStack.",
-                1000,
-                "Entusiasta de Machine Learning.",
-                1000,
-              ]}
-              speed={50}
-              repeat={Infinity}
-            />
-          </span>
-        </h3>
+      <div className="home-content">
+        <div className="home-text">
+          <h1 className="home-title">
+            Oi, aqui é a <span className="highlight-name">Isis</span>
+          </h1>
+          <h3 className="home-subtitle">
+            Sou{" "}
+            <span className="highlight">
+              <TypeAnimation
+                sequence={[
+                  "Cientista da Computação.",
+                  1000,
+                  "Dev FullStack.",
+                  1000,
+                  "Entusiasta de Machine Learning.",
+                  1000,
+                ]}
+                speed={50}
+                repeat={Infinity}
+              />
+            </span>
+          </h3>
+        </div>
+
+        <div className="skills-icons">
+          <div className="icon" id="typescript">
+            <i className="devicon-typescript-plain"></i>
+          </div>
+          <div className="icon" id="js">
+            <i className="devicon-javascript-plain"></i>
+          </div>
+          <div className="icon" id="html">
+            <i className="devicon-html5-plain"></i>
+          </div>
+          <div className="icon" id="css">
+            <i className="devicon-css3-plain"></i>
+          </div>
+          <div className="icon" id="csharp">
+            <i className="devicon-csharp-plain"></i>
+          </div>
+          <div className="icon" id="python">
+            <i className="devicon-python-plain"></i>
+          </div>
+          <div className="icon" id="git">
+            <i className="devicon-git-plain"></i>
+          </div>
+        </div>
+
+        <div className="scroll-down">
+          <i className="fa fa-chevron-down"></i>
+        </div>
+        
       </div>
-  
-      <div className="about-box">
-        <h3 className="about-subtitle">Quem sou eu?</h3>
-        <p>...</p>
-      </div>
-    </div>
-  
-    <div className="skills-icons">
-      <div className="icon" id="typescript">
-        <i className="devicon-typescript-plain"></i>
-      </div>
-      <div className="icon" id="js">
-        <i className="devicon-javascript-plain"></i>
-      </div>
-      <div className="icon" id="html">
-        <i className="devicon-html5-plain"></i>
-      </div>
-      <div className="icon" id="css">
-        <i className="devicon-css3-plain"></i>
-      </div>
-      <div className="icon" id="csharp">
-        <i className="devicon-csharp-plain"></i>
-      </div>
-      <div className="icon" id="python">
-        <i className="devicon-python-plain"></i>
-      </div>
-      <div className="icon" id="git">
-        <i className="devicon-git-plain"></i>
-      </div>
-    </div>
-  </section>
-  
+    </section>
   );
 };
 
