b49d1d7a736f7c15de2d74a812558ca27c6fd6e8
isisestevan <isisestevan@gmail.com>
2025-05-05 22:17:06 -0300

    fix: ajuste responsividade

--- meu-portfolio/src/components/Home.jsx
+++ meu-portfolio/src/components/Home.jsx
@@ -5,60 +5,60 @@ import "../styles/home.css";
 const Home = () => {
   return (
     <section className="home-section">
-      <div className="home-content">
-        <div className="home-text">
-          <h1 className="home-title">
-            Oi, aqui é a <span className="highlight-name">Isis</span>
-          </h1>
-          <h3 className="home-subtitle">
-            Sou{" "}
-            <span className="highlight">
-              <TypeAnimation
-                sequence={[
-                  "Cientista da Computação.",
-                  1000,
-                  "Dev FullStack.",
-                  1000,
-                  "Entusiasta de Machine Learning.",
-                  1000,
-                ]}
-                speed={50}
-                repeat={Infinity}
-              />
-            </span>
-          </h3>
-        </div>
+  <div className="home-content">
+    <div className="home-text">
+      <h1 className="home-title">
+        Oi, aqui é a <span className="highlight-name">Isis</span>
+      </h1>
+      <h3 className="home-subtitle">
+        Sou{" "}
+        <span className="highlight">
+          <TypeAnimation
+            sequence={[
+              "Cientista da Computação.",
+              1000,
+              "Dev FullStack.",
+              1000,
+              "Entusiasta de Machine Learning.",
+              1000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+          />
+        </span>
+      </h3>
+    </div>
+  </div>
 
-        <div className="skills-icons">
-          <div className="icon" id="typescript">
-            <i className="devicon-typescript-plain"></i>
-          </div>
-          <div className="icon" id="js">
-            <i className="devicon-javascript-plain"></i>
-          </div>
-          <div className="icon" id="html">
-            <i className="devicon-html5-plain"></i>
-          </div>
-          <div className="icon" id="css">
-            <i className="devicon-css3-plain"></i>
-          </div>
-          <div className="icon" id="csharp">
-            <i className="devicon-csharp-plain"></i>
-          </div>
-          <div className="icon" id="python">
-            <i className="devicon-python-plain"></i>
-          </div>
-          <div className="icon" id="git">
-            <i className="devicon-git-plain"></i>
-          </div>
-        </div>
+  <div className="skills-icons">
+    <div className="icon" id="typescript">
+      <i className="devicon-typescript-plain"></i>
+    </div>
+    <div className="icon" id="js">
+      <i className="devicon-javascript-plain"></i>
+    </div>
+    <div className="icon" id="html">
+      <i className="devicon-html5-plain"></i>
+    </div>
+    <div className="icon" id="css">
+      <i className="devicon-css3-plain"></i>
+    </div>
+    <div className="icon" id="csharp">
+      <i className="devicon-csharp-plain"></i>
+    </div>
+    <div className="icon" id="python">
+      <i className="devicon-python-plain"></i>
+    </div>
+    <div className="icon" id="git">
+      <i className="devicon-git-plain"></i>
+    </div>
+  </div>
 
-        <div className="scroll-down">
-          <i className="fa fa-chevron-down"></i>
-        </div>
-        
-      </div>
-    </section>
+  <div className="scroll-down">
+    <i className="fa fa-chevron-down"></i>
+  </div>
+</section>
+
   );
 };
 
